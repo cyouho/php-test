cd4d33d30a03f5c0a516ba4762fc5e7d87d825ab
zhangfan <cyouho@hotmail.co.jp>
2021-10-01 10:46:16 +0800

    test code

--- php_files/2021/09/30/php01.php
+++ php_files/2021/09/30/php01.php
@@ -1,16 +1,25 @@
 <?php
-$s = 'abcabcbb';
+$s = 'bbb';
 $arrs = str_split($s);
 $n = count($arrs);
-$m = count($arrs);
+
+$max = 0;
 
 for ($i = 0; $i < $n; $i++) {
-	for ($j = 0; $j < $n - $i; $j++) {
-		$pos[$j] = $arrs[$i];
-		if (in_array($arrs[$i + 1], $pos)) {
-			var_dump($i + 1);
+	for ($j = 0, $k = $i; $j < $n - $i; $j++, $k++) {
+		$pos[$j] = $arrs[$k];
+		if (in_array($arrs[$k + 1], $pos)) {
+			break;
 		}
 	}
+	$temp = count($pos);
+	if ($max < $temp) {
+		$max = $temp;
+	}
+	//var_dump($temp);
+	//var_dump($pos);
+	unset($pos);
 }
 
-var_dump($count);
+var_dump($max);
+//var_dump($count);
---
+++ php_files/2021/09/30/php02.php
@@ -0,0 +1,5 @@
+<?php
+$a = [1, 2, 3];
+var_dump($a);
+unset($a);
+var_dump($a);
