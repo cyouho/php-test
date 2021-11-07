9f2314c390a30b94f89bdd743643a7c94f896cc5
zhangfan <cyouho@hotmail.co.jp>
2021-11-07 22:18:19 +0800

    add code test

--- php_files/2021/11/php2.php
+++ php_files/2021/11/php2.php
@@ -1,4 +1,25 @@
 <?php
 $a = [];
-array_pad($a, 7, 0);
+$a = array_pad($a, 7, 0);
 var_dump($a);
+
+/*
+result:
+
+array(7) {
+  [0] =>
+  int(0)
+  [1] =>
+  int(0)
+  [2] =>
+  int(0)
+  [3] =>
+  int(0)
+  [4] =>
+  int(0)
+  [5] =>
+  int(0)
+  [6] =>
+  int(0)
+}
+*/
